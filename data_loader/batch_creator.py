02c8c96e19f76d0b65225248d031bf5e749cda6b
Darya Vinogradova <41550827+DaraOrange@users.noreply.github.com>
2021-04-09 19:39:14 +0300

    Create batch_creator.py

---
+++ data_loader/batch_creator.py
@@ -0,0 +1,68 @@
+import json
+import os
+import numpy as np
+
+class BatchCreator:
+    # too slow, will be improved  
+
+    def __init__(self, bs, train_path, images_path):
+        self.batch_size = bs
+        self._extract_text_and_images(train_path, images_path)
+        self._setup_data(train_path)
+        
+    def _setup_data(self, data_path):
+        self.idx_to_ep = {}
+        self.idx_to_turn = {}
+        
+        ep_idx = 0
+        for i, d in enumerate(self.data):
+            for j in range(len(d['dialog'])):
+                self.idx_to_ep[ep_idx] = i
+                self.idx_to_turn[ep_idx] = j 
+                ep_idx += 1
+                
+        self.total = ep_idx
+        
+    def _extract_text_and_images(self, train_path, images_path):
+        img_loader = ImageLoader({
+            'image_mode': 'resnet152',
+            'image_size': 256,
+            'image_cropsize': 224,
+        })
+        
+        raw_data = []
+        with open(train_path) as f:
+            raw_data = json.load(f)
+        
+        self.data = []
+        possible_hashes = set(os.listdir(images_path))
+        for i, sample in enumerate(raw_data[:50]):
+            print(i)
+            if sample['image_hash']+'.jpg' not in possible_hashes:
+                continue
+            sample['image'] = img_loader.load(os.path.join(images_path, sample['image_hash']+'.jpg'))
+            self.data.append(sample)
+        
+    def _get_dialogue(self, episode_idx):
+        print(self.idx_to_ep)
+        data = self.data[self.idx_to_ep[episode_idx]]
+        turn = self.idx_to_turn[episode_idx]
+        
+        personality, text = data['dialog'][turn]
+        episode_done = (turn == len(data['dialog'])-1)
+        full_dialog = [personality]
+        for i, utt in enumerate(data['dialog']):
+            if i >= turn:
+                break
+            full_dialog.append(utt[1])
+        return {
+            'text': '\n'.join(full_dialog),
+            'image': data['image'],
+            'episode_done': episode_done,
+            'labels': [text],
+        }
+    
+    def form_batch(self):
+        idxs = np.random.randint(0, self.total, size=self.batch_size)
+        raw_batch = [self._get_dialogue(idx) for idx in idxs]
+        return raw_batch
